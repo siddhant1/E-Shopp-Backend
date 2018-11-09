34b8233f9e6313d6e7badd5b49f585f9f26d5db5
siddhant1 <sidhant.manchanda@gmail.com>
2018-11-09 18:49:54 +0530

    Pagination Added

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { forwardTo } = require("prisma-binding");
 const Query = {
   items: forwardTo("db"),
-  item: forwardTo("db")
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db")
   // items(parent, args, { db }, info) {
   //   return db.query.items(null, info);
   // }
